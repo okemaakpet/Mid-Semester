4d6f0e78b047cedd8b6c22b1d81b392f4ebd9282
okemaakpet <153327004+okemaakpet@users.noreply.github.com>
2023-12-11 17:27:24 +0100

    Update question 2.cpp

--- question 2.cpp
+++ question 2.cpp
@@ -1,1 +1,11 @@
+print("Palindrome cChecker")
 
+user_input = input("Write a word")
+  
+if (user_input[0:] == user_input[:0]:)
+
+    print("This word is a Palindrome")
+
+else:
+  
+    print("This isn't  a Palindrome")
